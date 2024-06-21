5262ce209f032c4eda48e843ffa9564f7403318e
Dario Miranda <netobis2015@gmail.com>
2024-06-21 08:21:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/img/favicon2.webp" type="image/x-icon">
+    <title>Rede Canais HD</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <main>
+        <h1 class="titulo">HOME</h1>
+        <section id="container-conteudo">
+            <p class="letra-jap">家</p>
+
+            <div class="container-btn">
+                <a class="botao"
+                href="formulario_cadastro.php">Adicionar</a>
+                <a class="botao" href="catalogo.php">Catálogo</a>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
